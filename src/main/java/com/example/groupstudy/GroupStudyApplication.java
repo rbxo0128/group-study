ad25c07a7582fcbdbde591205095f0337a13d2eb
rbxo0128 <rbxo0128@naver.com>
2025-04-15 14:48:51 +0900

    first commit

---
+++ src/main/java/com/example/groupstudy/GroupStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.groupstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/groupstudy/GroupStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groupstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
